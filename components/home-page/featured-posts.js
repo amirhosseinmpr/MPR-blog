2577265c894804d0c1d8cb7cfb55f9ff9ccbceca
amir hoseein <amiirhossein.mpr@gmail.com>
2022-01-24 17:07:16 +0330

    featured post

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,2 +1,9 @@
-function contact() {}
+import classes from './featured-posts.module.css';
+function contact() {
+  return (
+    <section className={classes.latest}>
+      <h2> featured posts</h2>
+    </section>
+  );
+}
 export default contact;
