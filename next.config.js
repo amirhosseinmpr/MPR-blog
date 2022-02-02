5fe78d2b26a6b811ab2b7a8241ee640aa9afab1c
amir hoseein <amiirhossein.mpr@gmail.com>
2022-02-02 13:11:49 +0330

    Working With Environment Variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withPWA = require('next-pwa');
 module.exports = withPWA({
   reactStrictMode: true,
+  env: {
+    mongodb_username: 'USER',
+    mongodb_password: 'EjNUYvRsc8b8tEsK',
+    mongodb_cluster: 'cluster0',
+    mongodb_database: 'myFirstDatabase',
+  },
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -10,11 +10,10 @@ async function handler(req, res) {
     };
 
     let client;
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_cluster}.ji9cj.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
 
     try {
-      client = await MongoClient.connect(
-        'mongodb+srv://USER:EjNUYvRsc8b8tEsK@cluster0.ji9cj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-      );
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       res.status(500).json({ message: 'Could not connect to database.' });
       return;
